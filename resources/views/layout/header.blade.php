1331dd3c51e21ce06d2a08b334fd46df847a3b69
CARLOS <caetorrez@gmail.com>
2024-05-07 10:29:38 -0400

    Update header.blade.php

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -13,14 +13,14 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <div class="ms-auto">
-                <form action="{{}}" class="d-flex" style="min-width:500px">
+                <form action="" class="d-flex" style="min-width:500px">
                     <input class="form-control me-2" type="search" placeholder="Palabras claves" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Buscar</button>
                 </form>
             </div>
             <ul class="navbar-nav ms-2 mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{}}">
+                    <a class="nav-link" href="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload me-1" viewBox="0 0 16 16">
                             <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
                             <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z"/>
@@ -29,7 +29,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{}}">
+                    <a class="nav-link" href="">
                         Iniciar sesion
                     </a>
                 </li>
