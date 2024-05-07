c961989cf290a4fb7283b72894df6e6016635e03
alexviar <alexanderviar@gmail.com>
2024-05-07 04:21:25 -0400

    config: implementa el seeder para las facultades y carreras

--- app/Models/Carrera.php
+++ app/Models/Carrera.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Carrera extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "nombre"
+    ];
 }
--- app/Models/Facultad.php
+++ app/Models/Facultad.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Facultad extends Model
 {
     use HasFactory;
 
     public $table = "facultades";
+
+    protected $fillable = [
+        "nombre"
+    ];
+
+    #region Relationship
+    public function carreras():HasMany{
+        return $this->hasMany(Carrera::class);
+    }
+    #endregion
 }
