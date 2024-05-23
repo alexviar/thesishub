dc76251309f2ff0dbfbe5dd79486511773fe8a93
alexviar <alexanderviar@gmail.com>
2024-05-23 16:37:10 -0400

    fix: agrega el seeder facultades y carreras

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsuarioSeeder::class);
+        $this->call(FacultadYCarrerasSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
