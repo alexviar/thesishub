d83ee7d22887954e884382526d0f70c3baf790ce
alexviar <alexanderviar@gmail.com>
2024-05-23 20:38:56 -0400

    fix: migración

--- database/migrations/2024_05_15_201610_add_more_columns_to_usuarios_table.php
+++ database/migrations/2024_05_15_201610_add_more_columns_to_usuarios_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,7 +17,7 @@ return new class extends Migration
         //modifica una tabla usuarios en la base de datos
         Schema::table('usuarios',function (Blueprint $table) {
             $table->string('email')->nullable();
-            $table->boolean('is_admin')->defaultValue(false);
+            $table->boolean('is_admin')->default(false);
             $table->boolean('estado')->default(1);
             
             $table->unique('username');
@@ -32,8 +33,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('usuarios',function (Blueprint $table) {
-            $table->dropUnique('username');
-            $table->dropUnique('email');
+            $table->dropUnique(['username']);
+            $table->dropUnique(['email']);
             $table->dropColumn('email');
             $table->dropColumn('is_admin');
             $table->dropColumn('estado');
